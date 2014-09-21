9c8e676a8993cacb73e0811df3077826e3aedd07
Pedro Vereza <pdrvereza@gmail.com>
2014-09-21 11:27:31 -0300

    Finalizando AuthorizeCommand (é tetra)

--- src/bank/ui/text/command/AuthorizeTransferCommand.java
+++ src/bank/ui/text/command/AuthorizeTransferCommand.java
@@ -2,6 +2,7 @@ package bank.ui.text.command;
 
 import static java.lang.String.format;
 import bank.business.AccountManagementService;
+import bank.business.BusinessException;
 import bank.business.domain.Transfer;
 import bank.ui.text.BankTextInterface;
 import bank.ui.text.UIUtils;
@@ -10,6 +11,9 @@ import java.util.List;
 
 public class AuthorizeTransferCommand extends Command {
 
+    public static final String BACK_OPTION = "X";
+    public static final String AUTHORIZE_OPTION = "s";
+    public static final String CANCEL_OPTION = "n";
     private final AccountManagementService accountManagementService;
 
     public AuthorizeTransferCommand(BankTextInterface bankInterface, AccountManagementService accountManagementService) {
@@ -24,30 +28,48 @@ public class AuthorizeTransferCommand extends Command {
         List<Transfer> transfers = accountManagementService.getAllPendingTransfers();
 
         if (transfers.isEmpty()) {
-            System.out.println("message.nothing.pending");
+            System.out.println(getTextManager().getText("message.nothing.pending"));
             return;
         }
 
-        showSelectableTransfers(transfers);
+        Transfer transferToAuthorize = selectTransfer(ui, transfers);
 
-        int transferSelected = ui.readInteger(null);
-
-        if (transferSelected == 0)
+        if (transferToAuthorize == null) {
             return;
-
-        Transfer transferToAuthorize = transfers.get(transferSelected - 1);
-        System.out.println("message.option.authorize");
-
-        Transfer result;
-
-        if (ui.readString(null).equalsIgnoreCase("s")) {
-            result = accountManagementService.authorize(transferToAuthorize);
         }
-        else {
-            result = accountManagementService.cancel(transferToAuthorize);
+
+        String option = readValidAuthorizeOption(ui);
+
+        if (option.equalsIgnoreCase(BACK_OPTION)) {
+            return;
         }
+
+        Transfer result = applyOptionToTransfer(option, transferToAuthorize);
 
         System.out.println(result);
+    }
+
+    private Transfer applyOptionToTransfer(String option, Transfer transferToAuthorize) throws BusinessException {
+        Transfer result;
+
+        if (option.equalsIgnoreCase(AUTHORIZE_OPTION)) {
+            result = accountManagementService.authorize(transferToAuthorize);
+        } else {
+            result = accountManagementService.cancel(transferToAuthorize);
+        }
+        return result;
+    }
+
+    private Transfer selectTransfer(UIUtils ui, List<Transfer> transfers) {
+        showSelectableTransfers(transfers);
+
+        int selectedIndex = ui.readInteger(null, 0, transfers.size());
+
+        if (selectedIndex == 0) {
+            return null;
+        }
+
+        return transfers.get(selectedIndex - 1);
     }
 
     private void showSelectableTransfers(List<Transfer> transfers) {
@@ -55,6 +77,24 @@ public class AuthorizeTransferCommand extends Command {
             System.out.println(format("%d - %s", i + 1, transfers.get(i)));
         }
 
-        System.out.println("message.select.transfer");
+        System.out.println(getTextManager().getText("message.select.transfer"));
+    }
+
+    private String readValidAuthorizeOption(UIUtils ui) {
+        String option;
+
+        do {
+            System.out.println(getTextManager().getText("message.option.authorize"));
+            option = ui.readString(null);
+
+        } while (!validOption(option));
+
+        return option;
+    }
+
+    private boolean validOption(String option) {
+        return option.equalsIgnoreCase(AUTHORIZE_OPTION) ||
+                option.equalsIgnoreCase(CANCEL_OPTION) ||
+                option.equalsIgnoreCase(BACK_OPTION);
     }
 }
